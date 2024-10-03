c0b61f1024c6b2845de77637e407e2f0e135d820
JoanWolf <dasstian@hotmail.com>
2024-10-03 02:40:27 -0500

    Ceación clase DebugWriters para imprimir en output

---
+++ NorthWind.Writers/DebugWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace NorthWind.Writers
+{
+	public class DebugWriter  : IUserActionWriter
+	{
+		public void Write(UserAction action)
+		{
+			Debug.WriteLine("ConsoleWriter: {0}, {1}, {2}",
+				action.CreatedDatetime, action.User, action.Description);
+		}
+	}
+}
