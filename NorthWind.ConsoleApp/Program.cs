e18583f979977a572c606e247285cc7d2796e9b0
JoanWolf <dasstian@hotmail.com>
2024-10-03 02:41:10 -0500

    actualizaciones de codigo para implementar las nuevas clases

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -10,3 +10,25 @@ Logger.WriteLog("Application started");
 
 ProductService Service = new ProductService(Writer);
 Service.Add("Demo", "Azucar refinada");
+
+
+// --------------Implementación de FieWriter ----------------------
+
+IUserActionWriter DebugWriter = new DebugWriter();
+
+AppLogger LoggerDebugWriter = new AppLogger(DebugWriter);
+LoggerDebugWriter.WriteLog("Application started");
+
+ProductService ServiceDebugWriter = new ProductService(DebugWriter);
+ServiceDebugWriter.Add("Demo", "Azucar refinada");
+
+
+// --------------Implementación de FieWriter ----------------------
+
+IUserActionWriter FileWriter = new FileWriter();
+
+AppLogger LoggerFileWriter = new AppLogger(FileWriter);
+LoggerFileWriter.WriteLog("Application started");
+
+ProductService ServiceFileWriter = new ProductService(FileWriter);
+ServiceFileWriter.Add("Demo", "Azucar refinada");
